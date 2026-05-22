0923fd99017a9fd309643c2eac1492cd48dc068c
IT-RAYMOND\Raymond <raymond.b@anglosingapore.ac.th>
2026-05-22 13:29:54 +0700

    Fix PostgreSQL SSL connection for Render

--- service-code/src/db/index.ts
+++ service-code/src/db/index.ts
@@ -8,6 +8,9 @@ console.log(process.env.DATABASE_URL);
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export const db = drizzle(pool);
